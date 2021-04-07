9baf594d02632952568b6d78a324aa322bd81c36
Ernildo <ernildopnf@gmail.com>
2021-04-07 17:07:52 -0300

    Arquivos controllers.js e DataInfo.js refatorados

--- src/DataInfo.js
+++ src/DataInfo.js
@@ -1,48 +1,84 @@
 String.prototype.between = function(arg1, arg2) {
    
-   const startIndex = this.indexOf(arg1)
-   let str = null
+   const startIndex = this.indexOf(arg1);
+   let str = null;
 
    if (startIndex >= 0) {
       str = this.substring(startIndex)
-      const endIndex = str.indexOf(arg2)
+      const endIndex = str.indexOf(arg2);
 
       if (endIndex >= 0)
-         str = str.substring(0, endIndex + arg2.length)
+         str = str.substring(0, endIndex + arg2.length);
    }
 
-   return str
-}
+   return str;
+};
 
-function DataInfo(markup) {
+const getTitle = function private_getOtherTitle(markup = '') {
+   
+   const title = markup
+      .between('header-product__title', '</h1>')
+      .between('>', '<')
+      .replace('> ', '')
+      .replace(' <', '');
+   
+   return title;
+};
+
+function DataInfo(markup = '') {
 
    const dta = JSON.parse(markup
       .between('data-product', "}'>")
       .replace("data-product='", '')
-      .replace("'>", ''))
-
-   const { fullTitle, priceTemplate, baseUrl, variationPath, imageUrl } = dta
+      .replace("'>", '')
+   );
    
    const getName = () => {
-      if (fullTitle) return fullTitle
+      const { fullTitle } = dta;
+      return (fullTitle || getTitle(markup));
+   };
+
+   const getAvailability = () => {
+      const { fullTitle } = dta;
+      return (fullTitle ? true : false);
+   };
+
+   const getPrice = () => {
+      const { priceTemplate } = dta;
+
+      try {
+         return priceTemplate.replace(' ', '');
+      } catch(e) {
+         return '';
+      }
+   };
+
+   const getImage = () => {
+      const { imageUrl } = dta;
+      return imageUrl.replace('{w}x{h}', '618x463');
+   };
+
+   const getUrl = () => {
+      const { variationPath, baseUrl } = dta;
+      let stringUrl = null; 
       
-      return markup.between('header-product__title', '</h1>')
-                   .between('>', '<')
-                   .replace('> ', '')
-                   .replace(' <', '')
-   }
-   const getAvailability = () => fullTitle ? true : false
-   const getPrice = () => priceTemplate ? priceTemplate.replace(' ', '') : ''
-   const getImage = () => imageUrl.replace('{w}x{h}', '618x463')
-   const getUrl = () => variationPath ? `${baseUrl}/${variationPath}` : null
+      if (variationPath && baseUrl)
+         stringUrl = `${baseUrl}/${variationPath}`;
+      
+      return stringUrl;
+   };
 
    return {
-      getName,
-      getPrice,
-      getImage,
-      getUrl,
-      getAvailability
-   }
+      get availability() { return getAvailability(); },
+      
+      get price() { return getPrice(); },
+
+      get image() { return getImage(); },
+      
+      get name() { return getName(); },
+      
+      get url() { return getUrl(); }
+   };
 }
 
-module.exports = DataInfo
+module.exports = DataInfo;
--- src/controllers.js
+++ src/controllers.js
@@ -1,29 +1,27 @@
-const DataInfo = require('./DataInfo')
-const fetch = require('node-fetch')
+const DataInfo = require('./DataInfo');
+const fetch = require('node-fetch');
 
-function status(_, res) {
-   res.status(200).send('ok')
-}
+const status = (_, res) => {
+   res.status(200).send('ok');
+};
 
-function scrap(req, res) {
-   const { url } = req.body
+const scrap = (req, res) => {
+
+   const { url } = req.body;
 
    fetch(url)
       .then(response => response.text())
       .then(markup => {
-         const data = DataInfo(markup)
-
+         const data = DataInfo(markup);
+         
          const object = {
-            name: data.getName(),
-            imagePrimary: data.getImage(),
-            price: data.getPrice(),
-            availability: data.getAvailability(),
-            url: data.getUrl() || url
-         }
-
-         res.status(200).json(object)
+            ...data,
+            url: data.url || url
+         };
+         
+         res.status(200).json(object);
       })
-      .catch(() => res.status(500).send('problema desconhecido'))
-}
+      .catch(() => res.status(500).send('problema desconhecido'));
+};
 
 module.exports = { status, scrap }
